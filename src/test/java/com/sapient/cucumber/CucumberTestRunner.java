d230b500ef2ba02a6f7af5c91ca26dacc53dfa6d
Sachin Vitonde <svitonde@sapient.com>
2020-04-20 19:13:09 -0400

    refactored test

--- src/test/java/com/sapient/cucumber/CucumberTestRunner.java
+++ src/test/java/com/sapient/cucumber/CucumberTestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
  * Cucumber Options
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features/create-user.feature"},
+@CucumberOptions(features = {"src/test/resources/features"},
     plugin = {"pretty", "html:target/cucumber"}, glue = {"com.sapient.cucumber.stepdefs"})
 public class CucumberTestRunner {
 
--- src/test/java/com/sapient/cucumber/stepdefs/CreateUserStepDef.java
+++ src/test/java/com/sapient/cucumber/stepdefs/CreateUserStepDef.java
@@ -1,36 +1,24 @@
 package com.sapient.cucumber.stepdefs;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.util.List;
-import java.util.Map;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import com.sapient.cucumber.CucumberTestContextConfig;
 import com.sapient.cucumber.entity.User;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
-import cucumber.api.java8.En;
 import io.cucumber.datatable.DataTable;
 
-@AutoConfigureMockMvc
-// @ContextConfiguration
-// @SpringBootTest(classes = CucumberApplication.class)
-public class CreateUserStepDef extends BaseStepDef implements En {
+public class CreateUserStepDef extends CucumberTestContextConfig {
 
   private String apiUri;
   private User givenUser;
 
-  public CreateUserStepDef() {
-
-    // Given("^Create User using below data:$", (final List<User> userList) -> {
-    // assertNotNull(userList);
-    // givenUser = userList.get(0);
-    // apiUri = staticURL + port + "/cucumberdemo/v1/user/add";
-    // });
-
-
-  }
-
-
   @Given("^create user using following fields:$")
-  public void createUser(final DataTable dataTable) throws Exception {
+  public void createUser(final DataTable dataTable) throws Throwable {
 
     final List<List<String>> data = dataTable.asLists(String.class);
 
@@ -41,16 +29,23 @@ public class CreateUserStepDef extends BaseStepDef implements En {
     givenUser.setLastName(data.get(3).get(1));
     givenUser.setEmail(data.get(4).get(1));
 
-    apiUri = staticURL + port + "/cucumberdemo/v1/user/add";
+    apiUri = "/v1/user/add";
   }
 
-
   @When("^Saved successfully$")
-  public void saveUser() throws Exception {
+  public void saveUser() throws Throwable {
 
-    final Map<String, Object> map = testRestTemplate.postForObject(apiUri, givenUser, Map.class);
+    final MvcResult mvcResult = mockMvc.perform(post(apiUri)//
+        .content(mapper.writeValueAsString(givenUser))//
+        .contentType(MediaType.APPLICATION_JSON)//
+        .accept(MediaType.APPLICATION_JSON))//
+        .andDo(print())//
+        .andExpect(status().isOk())//
+        .andReturn();//
 
-    System.out.println(map.size());
+    System.out.println(mvcResult.getResponse().getContentAsString());
+
+
   }
 
 
