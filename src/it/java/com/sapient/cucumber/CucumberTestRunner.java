355756f68a3165bc760781abb61eadc3ecad9bf9
Sachin Vitonde <svitonde@sapient.com>
2020-04-21 12:48:26 -0400

    code refacored

--- src/it/java/com/sapient/cucumber/CucumberTestRunner.java
+++ src/it/java/com/sapient/cucumber/CucumberTestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
  * Cucumber Options
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features"},
+@CucumberOptions(features = {"src/it/resources/features"},
     plugin = {"pretty", "html:target/cucumber"}, glue = {"com.sapient.cucumber.stepdefs"})
 public class CucumberTestRunner {
 
